d432e0aa47858648703829cf6c52497f714381e0
Oco <92460234+nachodecores@users.noreply.github.com>
2024-11-07 16:18:15 -0300

    header mas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,28 +19,24 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full transition-all duration-500 ease-in-out z-50">
-      <div className="flex items-center justify-center md:justify-start max-w-6xl mx-auto py-4 px-6">
-        {/* Título "CAMPANA" centrado */}
-        <h1
-          className={`text-4xl font-bold transition-opacity duration-500 ease-in-out ${
-            isScrolled ? "opacity-0" : "opacity-100"
-          }`}
-        >
-          CAMPANA
-        </h1>
-
-        {/* Logo a la izquierda cuando se hace scroll */}
-        <div
-          className={`transition-opacity ease-in-out ${
-            isScrolled ? "opacity-100 ml-0 md:ml-4" : "opacity-0"
-          }`}
-        >
-          <img
-            src="/logo-campana-oscuro.svg"
-            alt="Logo"
-            className="h-20 w-20"
-          />
-        </div>
+      <div className="flex items-center justify-center max-w-6xl mx-auto py-4 px-6">
+        {isScrolled ? (
+          <div
+            className={`transition-opacity ease-in-out opacity-100 ml-0 md:ml-4 flex items-start`}
+          >
+            <img
+              src="/logo-campana-oscuro.svg"
+              alt="Logo"
+              className="h-20 w-20"
+            />
+          </div>
+        ) : (
+          <h1
+            className={`text-4xl font-bold transition-opacity duration-500 ease-in-out opacity-100`}
+          >
+            C A M P A N A
+          </h1>
+        )}
       </div>
     </header>
   );
